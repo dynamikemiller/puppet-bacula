6c9dd515bbff5660eb5a41f4f40e76a8cbf2e41f
Zach Leslie <xaque208@gmail.com>
2017-03-05 18:41:27 -0800

    Rubocop

--- spec/defines/job_spec.rb
+++ spec/defines/job_spec.rb
@@ -5,48 +5,49 @@ describe 'bacula::job' do
     context "on #{os}" do
       let(:facts) { facts }
 
-      let(:pre_condition) {
+      let(:pre_condition) do
         'include bacula::client'
-      }
+      end
 
       context 'A simple files job' do
         let(:title) { 'Etc' }
-        let(:params) { {
-          files: ['/etc']
-        } }
+        let(:params) do
+          {
+            files: ['/etc']
+          }
+        end
 
         it { is_expected.to contain_bacula__job('Etc') }
-        it {
+        it do
           expect(exported_resources).to contain_bacula__director__job('Etc').with_content(
             %r{Job \{\n.*Name.*=.*Etc\n.*Type.*= Backup}
           )
-        }
+        end
 
-        it {
+        it do
           expect(exported_resources).to contain_bacula__director__job('Etc').with_content(
             %r{FileSet.*=.*Etc}
           )
-        }
+        end
 
-        it {
+        it do
           expect(exported_resources).to contain_bacula__director__fileset('Etc').with_files(
             ['/etc']
           )
-        }
-
+        end
       end
 
       context 'with job_tag' do
         let(:title) { 'Etc' }
-        let(:params) { {
-          files: ['/etc'],
-          job_tag: 'simple',
-        } }
+        let(:params) do
+          {
+            files: ['/etc'],
+            job_tag: 'simple'
+          }
+        end
 
         it { is_expected.to contain_bacula__job('Etc') }
       end
-
     end
   end
 end
-
