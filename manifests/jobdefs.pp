aad3a1c8926d68e794295b831bb230ee8ee945f9
Michael Kunz <m.kunz@ixolit.com>
2017-03-09 15:57:51 +0100

    jobdefs: parameters for pool change on backup level upgrade

--- manifests/jobdefs.pp
+++ manifests/jobdefs.pp
@@ -4,6 +4,9 @@
 #
 # Parameters:
 #
+# full_backup_pool => http://wiki.bacula.org/doku.php?id=bacula_manual:the_job_resource#full_backup_pool_pool-resource-name
+# differential_backup_pool => http://wiki.bacula.org/doku.php?id=bacula_manual:the_job_resource#differential_backup_pool_pool-resource-name
+#
 # Actions:
 #
 # Requires:
@@ -11,16 +14,18 @@
 # Sample Usage:
 #
 define bacula::jobdefs (
-  $jobtype             = 'Backup',
-  $sched               = 'Default',
-  $messages            = 'Standard',
-  $priority            = '10',
-  $pool                = 'Default',
-  $level               = undef,
-  $accurate            = 'no',
-  $reschedule_on_error = false,
-  $reschedule_interval = '1 hour',
-  $reschedule_times    = '10',
+  $jobtype                  = 'Backup',
+  $sched                    = 'Default',
+  $messages                 = 'Standard',
+  $priority                 = '10',
+  $pool                     = 'Default',
+  $full_backup_pool         = undef,
+  $differential_backup_pool = undef,
+  $level                    = undef,
+  $accurate                 = 'no',
+  $reschedule_on_error      = false,
+  $reschedule_interval      = '1 hour',
+  $reschedule_times         = '10',
 ) {
 
   validate_re($jobtype, ['^Backup', '^Restore', '^Admin', '^Verify', '^Copy', '^Migrate'])
